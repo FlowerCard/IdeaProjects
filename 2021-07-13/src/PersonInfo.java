22750cd93272b124d9489c8d7c18e9d13361eded
FlowrCard <strangeralways@foxmail.com>
2021-08-07 16:12:19 +0800

    2021-07-13千锋作业

---
+++ 2021-07-13/src/PersonInfo.java
@@ -0,0 +1,14 @@
+public class PersonInfo {
+    public static void main(String[] args) {
+        String name = "张三丰";
+        int age = 22;
+        String sex = "男";
+        String hob = "编程，唱歌";
+        String xueLi = "本科";
+        String school = "东北林业大学";
+        String zhuanYe = "计算机科学与技术";
+        String homeAddress = "黑龙江省哈尔滨市南岗区学府路251号";
+        System.out.println(name + "\t" + age + "\t" + sex + "\t" + hob + "\t" + xueLi + "\t" + school + "\t" + zhuanYe
+                            + "\t" + homeAddress);
+    }
+}
