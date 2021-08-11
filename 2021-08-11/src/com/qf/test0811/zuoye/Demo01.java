24f865f58655eefb8f5b9076867daecbf1025ab6
FlowrCard <1274026997@qq.com>
2021-08-11 19:43:53 +0800

    初始化一个长度为5的二维数组,然后使用多线程计算总和,要求每条线程计算一个一维数组
    更新数组的赋值方式
    
    其中一个数组长度随机生成
    
    修改注释

--- 2021-08-11/src/com/qf/test0811/zuoye/Demo01.java
+++ 2021-08-11/src/com/qf/test0811/zuoye/Demo01.java
@@ -68,8 +68,6 @@ public class Demo01 {
                     System.out.println(Thread.currentThread().getName() + " -----> " + sum);
                     System.out.println("----------------------------------------");
 
-//                    System.out.println(sumArr);
-//                    System.out.println("----------------------------------------");
                 }
             });
         }
