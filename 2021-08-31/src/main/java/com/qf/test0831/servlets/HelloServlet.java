e958153e8783b4dab626991abb0d39eb6f088013
FlowrCard <1274026997@qq.com>
2021-08-31 11:37:20 +0800

    Tomcat + Servlet 快速入门

---
+++ 2021-08-31/src/main/java/com/qf/test0831/servlets/HelloServlet.java
@@ -0,0 +1,36 @@
+package com.qf.test0831.servlets;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author HuaPai
+ * @email HuaPai@odcn.live
+ * Created on 2021/8/31.
+ */
+public class HelloServlet implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("服务器启动。。。");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
