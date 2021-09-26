8a35157b127c902c6dd23781c276b4f46a68e709
FlowerCard <1274026997@qq.com>
2021-09-26 20:16:44 +0800

    Spring框架 依赖注入——数据源注入(底层使用set注入)

--- 2021-09-26/src/main/java/com/huapai/dao/impl/AccountDaoImpl.java
+++ 2021-09-26/src/main/java/com/huapai/dao/impl/AccountDaoImpl.java
@@ -2,6 +2,9 @@ package com.huapai.dao.impl;
 
 import com.huapai.dao.IAccountDao;
 import com.huapai.pojo.Account;
+import com.alibaba.druid.pool.DruidDataSource;
+
+import java.sql.SQLException;
 
 /**
  * @author HuaPai
@@ -9,8 +12,24 @@ import com.huapai.pojo.Account;
  * Created on 2021/9/26.
  */
 public class AccountDaoImpl implements IAccountDao {
+    
+    private DruidDataSource dataSource;
+
+    public void setDataSource(DruidDataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public DruidDataSource getDataSource() {
+        return dataSource;
+    }
+
     @Override
     public Account selectAccountById(Integer id) {
+        try {
+            System.out.println(dataSource.getConnection());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         Account account = new Account();
         account.setId(id);
         account.setUsername("test01");
