038803e55134ace193b93fb3481f09cb44b3891b
FlowerCard <1274026997@qq.com>
2021-10-02 11:23:45 +0800

    Spring MVC 传值，Session域传值

--- 2021-09-30_SpringMVC_Data_Forward/src/main/java/com/huapai/controller/DataController.java
+++ 2021-09-30_SpringMVC_Data_Forward/src/main/java/com/huapai/controller/DataController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author HuaPai
@@ -30,6 +31,26 @@ public class DataController {
         System.out.println("num ===> " + num);
         request.setAttribute("num",num);
         return "redirect:/data01.jsp";
+    }
+    
+    @RequestMapping("/update01")
+    public String updateData01(Integer num, String username, HttpSession session, HttpServletRequest request){
+        num += 66;
+        username = username + num;
+        System.out.println("num ===> " + num);
+        request.setAttribute("num",num);
+        session.setAttribute("username",username);
+        return "data02";
+    }
+    
+    @RequestMapping("/update02")
+    public String updateData02(Integer num, String username, HttpSession session, HttpServletRequest request){
+        num += 66;
+        username = username + num;
+        System.out.println("num ===> " + num);
+        request.setAttribute("num",num);
+        session.setAttribute("username",username);
+        return "redirect:/data02.jsp";
     }
     
 }
---
+++ 2021-09-30_SpringMVC_Data_Forward/src/main/webapp/data02.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ASUA
+  Date: 2021/10/2
+  Time: 11:15
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    num=${num}
+    <hr>
+    username=${username}
+</body>
+</html>
