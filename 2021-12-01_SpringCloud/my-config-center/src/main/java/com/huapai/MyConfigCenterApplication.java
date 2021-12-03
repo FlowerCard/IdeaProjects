f5d4e34b2eaab6cba60e65e3e068eca828d22b02
HuaPai <HuaPai@odcn.live>
2021-12-03 11:57:19 +0800

    Spring Cloud 分布式配置中心

---
+++ 2021-12-01_SpringCloud/my-config-center/src/main/java/com/huapai/MyConfigCenterApplication.java
@@ -0,0 +1,15 @@
+package com.huapai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MyConfigCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyConfigCenterApplication.class, args);
+    }
+
+}
